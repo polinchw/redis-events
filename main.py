5577da8e99ddb3b32eceb3a778983fc5e6d103cc
William Polinchak <william.polinchak@windstream.com>
2021-10-01 09:00:23 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,35 @@
+import redis
+import config
+import threading
+import time
+
+def callback(response, **options):
+    print('hi')
+
+client = redis.StrictRedis()
+pubsub = client.pubsub()
+
+# Set config in config file "notify-keyspace-events Ex"
+# Subscribing to key expire events and whenver we get any notification sending it to event_handler function
+pubsub.psubscribe(**{"__keyevent@0__:expired": callback})
+pubsub.run_in_thread(sleep_time=0.01)
+
+
+def alarm_missing_logic():
+    """
+    This logic will look at the heart beats in the timestamped records and compare it against the general inventory
+    and it will create alerts when necessary.
+    """
+    while True:
+        time.sleep(30)
+        client.set_response_callback('EXPIRE', callback)
+        client.set('a', 1)
+        client.expire('a',5)
+
+
+t1 = threading.Thread(target=alarm_missing_logic, args=[])
+t1.start()
+
+
+
+
